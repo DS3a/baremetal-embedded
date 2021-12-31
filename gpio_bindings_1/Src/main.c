22d0186d94e0903441b8e5041efa37d0752ae175
Deepesh Padala <padaladeepesh@gmail.com>
2021-12-31 17:04:10 +0530

    using bsrr instead of odr

--- gpio_bindings_1/Src/main.c
+++ gpio_bindings_1/Src/main.c
@@ -11,7 +11,20 @@ int main(void) {
 
 	GPIOA->MODER |= ((1U<<10) & (~(1U<<11)));
 	while(1) {
+
+		/*
 		GPIOA->ODR ^= LED_PIN;
+		for (int i=0; i<100000;i++) {}
+		*/
+
+		/* It's better to use BSRR instead.
+		 *  as we don't need to read it,
+		 *  and then modify and then reset it
+		 */
+		GPIOA->BSRR |= LED_PIN;
+		for (int i=0; i<100000;i++) {}
+
+		GPIOA->BSRR |= (LED_PIN<<16);
 		for (int i=0; i<100000;i++) {}
 	}
 }
